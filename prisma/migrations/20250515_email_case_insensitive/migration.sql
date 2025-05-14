bae14885fcd3f48dd018a1c9984f38a3e2919766
Jesse Hines <j4hines@uwaterloo.ca>
2025-05-14 12:51:07 -0400

    Resolve email case sensitivity migration

--- prisma/migrations/20250515_email_case_insensitive/migration.sql
+++
@@ -1,7 +0,0 @@
--- Create a unique index for email with case insensitivity
-DROP INDEX IF EXISTS "User_email_key";
-CREATE UNIQUE INDEX "User_email_key" ON "User" (LOWER("email"));
-
--- Add a database-level constraint to enforce case-insensitive uniqueness
-ALTER TABLE "User" ADD CONSTRAINT "User_email_case_insensitive" 
-UNIQUE USING INDEX "User_email_key";
